f313237d12917cc5eb64239e2ea26c277a0cd25d
its-hmtu <tu.hoangminh15@gmail.com>
2024-09-19 18:00:12 +0700

    update

--- server/app.js
+++ server/app.js
@@ -3,22 +3,14 @@ var viewConfig = require('./config/view');
 var errorConfig = require('./config/error');
 var utilitiesConfig = require('./config/utilities');
 var routesConfig = require('./config/routes');
-// var Sequelize = require('sequelize');
-const db = require('./config/database');
+const connectDB = require('./config/database');
 const cors = require('cors');
 var express = require('express');
 var app = express();
 
 app.use(cors());
 
-db.sequelize.authenticate()
-  .then(() =>{
-    console.log('Connected to database: ' + process.env.DB_DATABASE);
-  })
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  });
-
+connectDB();
 viewConfig(app);
 utilitiesConfig(app);
 routesConfig(app);
--- server/config/database.js
+++ server/config/database.js
@@ -1,4 +1,19 @@
-module.exports = require('../models');
+require('dotenv').config();
+const db = require('../models');
+const config = require('./config.json')[process.env.MODE || 'development'];
+
+function connectDB() {
+  db.sequelize.authenticate()
+    .then(() => {
+      console.log('Connected to database: ' + config.database);
+    })
+    .catch(err => {
+      console.error('Unable to connect to the database:', err);
+    });
+}
+
+module.exports = connectDB;
+
 
 
 
