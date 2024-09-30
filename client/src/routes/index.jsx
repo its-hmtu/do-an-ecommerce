89bc33bab14b14649e2cb4b14a6800cfed3d75d1
its-hmtu <tu.hoangminh15@gmail.com>
2024-10-01 00:42:50 +0700

    update path alias

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createBrowserRouter } from 'react-router-dom';
-import MainLayout from '../layouts/MainLayout';
+import MainLayout from 'layouts/MainLayout';
 
 const routes = createBrowserRouter([
     {
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import eslint from 'vite-plugin-eslint'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,24 @@ export default defineConfig({
     react(),
     eslint(),
   ],
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, "./src"),
+      components: path.resolve(__dirname, "./src/components"),
+      pages: path.resolve(__dirname, "./src/pages"),
+      assets: path.resolve(__dirname, "./src/assets"),
+      layouts: path.resolve(__dirname, "./src/layouts"),
+      utils: path.resolve(__dirname, "./src/utils"),
+      styles: path.resolve(__dirname, "./src/styles"),
+      images: path.resolve(__dirname, "./src/assets/images"),
+      svg: path.resolve(__dirname, "./src/assets/svg"),
+      api: path.resolve(__dirname, "./src/api"),
+      routes: path.resolve(__dirname, "./src/routes"),
+      data: path.resolve(__dirname, "./src/data"),
+      hooks: path.resolve(__dirname, "./src/utils/hooks"),
+      contexts: path.resolve(__dirname, "./src/contexts"),
+    },
+  },
   server: {
     proxy: {
       "/users": "http://localhost:5000",
